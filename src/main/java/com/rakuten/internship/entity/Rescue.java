fda896e0e22e9c4ee79df6cfd5ae748f45995d47
T45K <tasktas9@gmail.com>
2019-09-10 17:45:27 +0900

    impl get distance method

--- src/main/java/com/rakuten/internship/entity/Rescue.java
+++ src/main/java/com/rakuten/internship/entity/Rescue.java
@@ -37,4 +37,29 @@ public class Rescue {
     private String image;
 
     private boolean isSolved;
+
+    /**
+     * see https://www.wolframalpha.com/examples/everyday-life/todays-world/
+     * <p>
+     * a = sin²(Δφ/2) + cos φ1 ⋅ cos φ2 ⋅ sin²(Δλ/2)
+     * c = 2 ⋅ atan2( √a, √(1−a) )
+     * d = R ⋅ c
+     *
+     * @param latitude  緯度
+     * @param longitude 経度
+     * @return distance(km)
+     */
+    public double getDistance(final double latitude, final double longitude) {
+        final double radius = 6371;
+        final double latitudeDelta = this.latitude - latitude;
+        final double longitudeDelta = this.longitude - longitude;
+
+        final double sinOfLatitudeDelta = Math.sin(latitudeDelta / 2);
+        final double sinOfLongitudeDelta = Math.sin(longitudeDelta / 2);
+
+        final double a = Math.pow(sinOfLatitudeDelta, 2) + Math.cos(latitude) * Math.cos(this.latitude) * Math.pow(sinOfLongitudeDelta, 2);
+        final double b = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return radius * b;
+    }
 }
