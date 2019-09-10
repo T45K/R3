ab474db9280fccccf5dccb07eb6aa1cfc359fb9e
T45K <tasktas9@gmail.com>
2019-09-10 16:58:19 +0900

    rename

--- src/main/java/com/rakuten/internship/entity/Rescue.java
+++ src/main/java/com/rakuten/internship/entity/Rescue.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 
 @Entity
 @Data
-public class Request {
+public class Rescue {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/rakuten/internship/repository/RequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rakuten.internship.repository;
-
-import com.rakuten.internship.entity.Request;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestRepository extends JpaRepository<Request, Long> {
-    Request findRequestById(final long id);
-}
