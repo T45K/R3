65a91dffcc1eca56255fb8f21a4769db1c9783fc
T45K <tasktas9@gmail.com>
2019-09-10 12:53:46 +0900

    add service

---
+++ src/main/java/com/rakuten/internship/service/RequestService.java
@@ -0,0 +1,26 @@
+package com.rakuten.internship.service;
+
+import com.rakuten.internship.entity.Request;
+import com.rakuten.internship.repository.RequestRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class RequestService {
+    private final RequestRepository repository;
+
+    public RequestService(final RequestRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Request> findAllRequests() {
+        return repository.findAll();
+    }
+
+    public Request findRequestById(final long id) {
+        return repository.findRequestById(id);
+    }
+}
