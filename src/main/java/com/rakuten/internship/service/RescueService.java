82ce63af3a3cf49456536e5cc8a758232ab479cf
T45K <tasktas9@gmail.com>
2019-09-10 17:45:35 +0900

    add new service

--- src/main/java/com/rakuten/internship/service/RescueService.java
+++ src/main/java/com/rakuten/internship/service/RescueService.java
@@ -5,7 +5,9 @@ import com.rakuten.internship.repository.RescueRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -26,5 +28,12 @@ public class RescueService {
 
     public void save(final Rescue rescue) {
         repository.save(rescue);
+    }
+
+    public List<Rescue> findAllRescuesFilteredByPoint(final double latitude, final double longitude) {
+        return repository.findAll().stream()
+                .filter(rescue -> rescue.getDistance(latitude, longitude) < 10)
+                .sorted(Comparator.comparing(a -> a.getDistance(latitude, longitude)))
+                .collect(Collectors.toList());
     }
 }
