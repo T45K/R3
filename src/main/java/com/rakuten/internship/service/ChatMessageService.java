0130a51c1740a15277ecc29b2c921c4eb75c71b3
T45K <tasktas9@gmail.com>
2019-09-11 14:37:15 +0900

    impl service

---
+++ src/main/java/com/rakuten/internship/service/ChatMessageService.java
@@ -0,0 +1,22 @@
+package com.rakuten.internship.service;
+
+import com.rakuten.internship.entity.ChatMessage;
+import com.rakuten.internship.repository.ChatMessageRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ChatMessageService {
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChatMessageService(final ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
+    }
+
+    public List<ChatMessage> findAllByRescueId(final long rescueId) {
+        return chatMessageRepository.findAllByRescueId(rescueId);
+    }
+}
